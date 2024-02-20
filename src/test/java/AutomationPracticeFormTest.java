1f977d5b2d0c03dd9629cf2a796b42d236907e3f
fnoskov <Feliks.Noskov@vsegda-da.com>
2024-02-20 08:58:01 +0400

    fixes after review

--- src/test/java/AutomationPracticeFormTest.java
+++ src/test/java/AutomationPracticeFormTest.java
@@ -1,53 +1,78 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.util.Calendar;
-
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeFormTest extends BaseTest {
 
-    private static final String BASE_PATH = "/automation-practice-form";
+    private static final String AUTOMATION_PRACTICE_FORM = "/automation-practice-form";
 
 
     @Test
     public void fillAllFieldsTest() {
 
-        open(BASE_PATH);
+        open(AUTOMATION_PRACTICE_FORM);
+
+        var name = "Test";
+        var lastName = "Testov";
+        var email = "test@test.com";
+        var gender = "Male";
+        var phoneNumber = "9269500099";
+
+        var month = "March";
+        var year = "2000";
+        var day = "14";
+
+        var subject = "Maths";
+        var hobby = "Reading";
+        var docName = "blankDoc.pdf";
+        var address = "Test address";
+        var state = "Haryana";
+        var city = "Karnal";
 
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
-        $("#firstName").setValue("Test");
-        $("#lastName").setValue("Testov");
-        $("#userEmail").setValue("test@test.com");
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("9269500099");
+
+        $("#firstName").setValue(name);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(phoneNumber);
 
         $("#dateOfBirthInput").click();
         $(".react-datepicker__header").shouldBe(Condition.visible);
-        $(".react-datepicker__month-select").selectOption("March");
+        $(".react-datepicker__month-select").selectOption(month);
 
-        var currentYear = Integer.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__month").$(byText(day)).click();
 
-        $(".react-datepicker__year-select").selectOption(currentYear.toString());
-        $(".react-datepicker__month").$(byText("14")).click();
+        $("#subjectsInput").setValue(subject).pressEnter();
 
-        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#hobbiesWrapper").$(byText(hobby)).click();
 
-        $("#hobbiesWrapper").$(byText("Reading")).click();
-
-        $("#uploadPicture").uploadFile(new File("src/test/resources/documents/blankDoc.pdf"));
-        $("#currentAddress").setValue("Test address");
+        $("#uploadPicture").uploadFromClasspath("documents/" + docName);
+        $("#currentAddress").setValue(address);
 
         $("#stateCity-wrapper").$("#state").click();
-        $("#state").$(byText("Haryana")).click();
+        $("#state").$(byText(state)).click();
 
         $("#stateCity-wrapper").$("#city").click();
-        $("#city").$(byText("Karnal")).click();
+        $("#city").$(byText(city)).click();
 
         $("#submit").click();
 
+
+        $(".table").shouldHave(text(name + " " + lastName));
+        $(".table").shouldHave(text(email));
+        $(".table").shouldHave(text(gender));
+        $(".table").shouldHave(text(phoneNumber));
+        $(".table").shouldHave(text(String.format("%s %s,%s", day, month, year)));
+        $(".table").shouldHave(text(subject));
+        $(".table").shouldHave(text(hobby));
+        $(".table").shouldHave(text(docName));
+        $(".table").shouldHave(text(address));
+        $(".table").shouldHave(text(String.format("%s %s", state, city)));
     }
 }
